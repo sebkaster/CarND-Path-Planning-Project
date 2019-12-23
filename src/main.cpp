9843969dcd3589adc324831c54db9fabbe81ffc2
sebkaster <sebastiankaster@googlemail.com>
2019-12-23 02:09:37 +0100

    parallized code

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,9 @@
 #include "helpers.h"
 #include "json.hpp"
 #include "spline.h"
-
+#include <omp.h>
 
 #include "car.h"
-
-// for convenience
-using nlohmann::json;
-using std::string;
-using std::vector;
 
 /* define constants */
 const double TIME_STEP_SIZE = 0.02;
@@ -24,6 +19,8 @@ const double SPEED_LIMIT = 22; // m/s
 const double MAX_ACC = 5.0 * TIME_STEP_SIZE; // m/s^2
 const double SAFETY_DISTANCE = 20;
 const double SPEED_BUFFER = 5.0;
+const unsigned NUM_THREADS = 4;
+
 
 int main() {
     uWS::Hub h;
@@ -79,7 +76,7 @@ int main() {
             auto s = hasData(data);
 
             if (s != "") {
-                auto j = json::parse(s);
+                auto j = nlohmann::json::parse(s);
 
                 string event = j[0].get<string>();
 
@@ -107,7 +104,7 @@ int main() {
                     //   of the road.
                     auto sensor_fusion = j[1]["sensor_fusion"];
 
-                    json msgJson;
+                    nlohmann::json msgJson;
 
                     size_t prev_size = previous_path_x.size();
 
@@ -181,16 +178,32 @@ int main() {
                     Car ego_car(s, s_d, d, d_d, middle);
                     ego_car.determineLane();
 
+                    double max_add_on_s = 0.0;
+                    double current_vel = car_speed;
+                    for (size_t i = 0; i < NUM_TIME_STEPS - prev_size; ++i) {
+                        current_vel += car_speed + MAX_ACC;
+                        max_add_on_s += current_vel * TIME_STEP_SIZE;
+                    }
+
+                    double safety_distance =
+                            0.55 * car_speed * 3.6;
+
                     bool car_ahead = false;
+                    bool car_ahead_dangerous = false;
                     bool could_change_left = true;
                     bool could_change_right = true;
+                    bool car_in_front_left = false;
+                    bool car_in_front_right = false;
 
                     if (ego_car.get_current_lane() == left) {
                         could_change_left = false;
                     } else if (ego_car.get_current_lane() == right) {
-                        could_change_right = true;
+                        could_change_right = false;
                     }
 
+                    #pragma omp parallel for schedule(guided) num_threads(OPEN_MP_NUM_THREADS) \
+                    reduction(&&, could_change_left) reduction(&&, could_change_right) reduction(||, car_ahead_dangerous) \
+                    reduction(||, car_ahead) reduction(||, car_in_front_right) reduction(||, car_in_front_left)
                     for (auto const &elem : sensor_fusion) {
                         Car new_car(elem[5], elem[3], elem[6], elem[4]);
                         new_car.determineLane();
@@ -200,53 +213,38 @@ int main() {
                         }
                         auto[closest_s, furthest_s] = new_car.predictFutureSates(NUM_TIME_STEPS);
 
-
-                        double max_add_on_s = 0.0;
-                        double current_vel = car_speed;
-                        for (size_t i = 0; i < NUM_TIME_STEPS - prev_size; ++i) {
-                            current_vel += car_speed + MAX_ACC;
-                            max_add_on_s += current_vel * TIME_STEP_SIZE;
-                        }
-
-                        double safety_distance =
-                                0.55 * car_speed * 3.6;
-
                         bool dangerous = (closest_s - (safety_distance + max_add_on_s)) < ego_car.get_s() &&
                                          ego_car.get_s() < (furthest_s + (safety_distance - max_add_on_s));
 
-                        if (dangerous) {
-
-                            if (new_car.get_current_lane() == ego_car.get_current_lane()) {
-                                if (ego_car.get_s() < new_car.get_s() &&
-                                    (car_speed + SPEED_BUFFER) > new_car.get_v_abs()) {
-                                    car_ahead = true;
-                                }
-                            } else {
-                                switch (ego_car.get_current_lane()) {
-                                    case middle: {
-                                        if (new_car.get_current_lane() == left) {
-                                            could_change_left = false;
-                                        } else if (new_car.get_current_lane() == right) {
-                                            could_change_right = false;
-                                        }
-                                        break;
-                                    }
-                                    case left: {
-                                        if (new_car.get_current_lane() == middle) {
-                                            could_change_right = false;
-                                        }
-                                        break;
-                                    }
-                                    case right: {
+                        if (new_car.get_current_lane() == ego_car.get_current_lane()) {
+                            if (ego_car.get_s() < new_car.get_s() && dangerous &&
+                                (car_speed + SPEED_BUFFER) > new_car.get_v_abs()) {
+                                car_ahead_dangerous = true;
+                            }
+                        } else {
+                            switch (ego_car.get_current_lane()) {
+                                case middle: {
+                                    if (new_car.get_current_lane() == left && dangerous) {
+                                        could_change_left = false;
+                                    } else if (new_car.get_current_lane() == right && dangerous) {
                                         could_change_right = false;
-                                        if (new_car.get_current_lane() == middle) {
-                                            could_change_left = false;
-                                        }
-                                        break;
                                     }
-                                    default: {
-                                        break;
+                                    break;
+                                }
+                                case left: {
+                                    if (new_car.get_current_lane() == middle && dangerous) {
+                                        could_change_right = false;
                                     }
+                                    break;
+                                }
+                                case right: {
+                                    if (new_car.get_current_lane() == middle && dangerous) {
+                                        could_change_left = false;
+                                    }
+                                    break;
+                                }
+                                default: {
+                                    break;
                                 }
                             }
                         }
@@ -255,21 +253,15 @@ int main() {
 
                     Lane next_lane = ego_car.get_current_lane();
                     bool changed_lane = false;
-                    if (car_ahead && (could_change_left || could_change_right)) {
+                    if (car_ahead_dangerous && (could_change_left || could_change_right)) {
 
                         next_lane = static_cast<Lane>(could_change_left ? int(next_lane) - 1 :
                                                       int(next_lane) + 1);
                         changed_lane = true;
-                    } else if (next_lane != middle && ((next_lane == left && could_change_right) ||
-                                                       (next_lane == right && could_change_left))) {
-                        next_lane == middle;
-                        changed_lane = true;
                     }
-                    std::cout << "left: " << could_change_left << std::endl;
-                    std::cout << "right: " << could_change_right << std::endl;
 
                     double speed_diff = 0;
-                    if (car_ahead && !changed_lane) {
+                    if (car_ahead_dangerous && !changed_lane) {
                         speed_diff -= MAX_ACC;
                     } else {
                         if (ref_vel < SPEED_LIMIT) {
@@ -277,15 +269,14 @@ int main() {
                         }
                     }
 
-                    std::cout << "next_lane: " << int(next_lane) << std::endl;
                     // Setting up target points in the future.
-                    vector<double> next_wp0 = getXY(car_s + 30, 2 + 4 * int(next_lane), map_waypoints_s,
+                    std::vector<double> next_wp0 = getXY(car_s + 30, 2 + 4 * int(next_lane), map_waypoints_s,
                                                     map_waypoints_x,
                                                     map_waypoints_y);
-                    vector<double> next_wp1 = getXY(car_s + 60, 2 + 4 * int(next_lane), map_waypoints_s,
+                    std::vector<double> next_wp1 = getXY(car_s + 60, 2 + 4 * int(next_lane), map_waypoints_s,
                                                     map_waypoints_x,
                                                     map_waypoints_y);
-                    vector<double> next_wp2 = getXY(car_s + 90, 2 + 4 * int(next_lane), map_waypoints_s,
+                    std::vector<double> next_wp2 = getXY(car_s + 90, 2 + 4 * int(next_lane), map_waypoints_s,
                                                     map_waypoints_x,
                                                     map_waypoints_y);
 
@@ -317,10 +308,8 @@ int main() {
                     std::vector<double> next_y_vals;
                     next_y_vals.reserve(NUM_TIME_STEPS);
 
-                    for (int i = 0; i < prev_size; i++) {
-                        next_x_vals.emplace_back(previous_path_x[i]);
-                        next_y_vals.emplace_back(previous_path_y[i]);
-                    }
+                    next_x_vals.insert(std::end(next_x_vals), std::begin(previous_path_x), std::end(previous_path_x));
+                    next_y_vals.insert(std::end(next_y_vals), std::begin(previous_path_y), std::end(previous_path_y));
 
                     // Calculate distance y position on 30 m ahead.
                     double target_x = 30.0;
